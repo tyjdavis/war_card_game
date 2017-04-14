29f2ecde7c47afe520bdb271f2e68f6eae825a9a
Tyler Davis <tyjdavis@outlook.com>
2017-04-14 15:11:00 -0400

    cleaning up vars

--- test/warTest.js
+++ test/warTest.js
@@ -21,18 +21,19 @@ const Deck = require('../lib/Deck');
 
 
 describe('War', function () {
+  let game;
+  beforeEach(function () {
+    game = new War('Tyler', 'Idiot');
+  })
   it('has two players', function() {
-    let game = new War('Tyler', 'Idiot');
     assert.equal(game.playerOne, 'Tyler');
     assert.equal(game.playerTwo, 'Idiot');
   })
   describe('#gameStart()', function (){
     it('playerOne has 26 cards', function (){
-      let game = new War('Tyler', 'Idiot');
       assert.equal(game.playerOneDeck.length, 26);
     })
     it('playerTwo has 26 cards', function (){
-      let game = new War('Tyler', 'Idiot');
       assert.equal(game.playerTwoDeck.length, 26)
     })
     describe('#getRawValues()', function(){
@@ -43,39 +44,32 @@ describe('War', function () {
     })
     describe('#showCards()', function (){
       it('playerOne shows first card', function (){
-        let game = new War('Tyler', 'Idiot');
-        let showCard = game.showCard1();
-        assert(showCard);
-        //console.log(showCard);
+        game.showCard1();
+        assert(game.showCard1());
       })
       it('playerTwo shows first card', function (){
-        let game = new War('Tyler', 'Idiot');
-        let showCard = game.showCard2();
-        assert(showCard);
-        //console.log(showCard);
+        game.showCard2();
+        assert(game.showCard2());
       })
     })
     describe('#whoWins()', function(){
       it('playerOne can win', function(){
-        let game = new War('Tyler', 'Idiot');
         game.playerOneDeck[0].rawValue = 10;
         game.playerTwoDeck[0].rawValue = 2;
-        let whoWins = game.whoWins();
-        assert.equal(whoWins, "Player one wins")
+        game.whoWins();
+        assert.equal(game.whoWins(), "Player one wins")
       })
       it('playerTwo can win', function (){
-        let game = new War('Tyler', 'Idiot');
         game.playerOneDeck[0].rawValue = 2;
         game.playerTwoDeck[0].rawValue = 10;
-        let whoWins = game.whoWins();
-        assert.equal(whoWins, "Player two wins")
+        game.whoWins();
+        assert.equal(game.whoWins(), "Player two wins")
       })
       it('a War can occur', function () {
-        let game = new War('Tyler', 'Idiot');
         game.playerOneDeck[0].rawValue = 10;
         game.playerTwoDeck[0].rawValue = 10;
-        let whoWins = game.whoWins();
-        assert.equal(whoWins, "War!")
+        game.whoWins();
+        assert.equal(game.whoWins(), "War!")
       })
     })
   })
