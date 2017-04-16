fc7da6961b0a82032cef418be957944d1513d6f9
Tyler Davis <tyjdavis@outlook.com>
2017-04-16 19:10:55 -0400

    cleaned up

--- test/warTest.js
+++ test/warTest.js
@@ -10,7 +10,7 @@ If both cards have the same value, "war" is declared. - done
 Each player places a second card face down on top of the first, and then a third, face up, on top of the second. -done
 The player with the high third card wins the round, and puts all six cards at the bottom of his/her hand. If the third cards tie, continue with another war.-done
 
-The winner is the player who ends up with all of the cards.
+The winner is the player who ends up with all of the cards. -done
 */
 
 
@@ -22,87 +22,81 @@ const Deck = require('../lib/Deck');
 
 describe('War', function () {
   let game;
+  let gameStart;
   beforeEach(function () {
     game = new War('Tyler', 'Idiot');
+    gameStart = game.gameStart();
   })
-  it('has two players', function() {
-    game.gameStart();
+  it('Game has two players', function() {
     assert.equal(game.playerOne, 'Tyler');
     assert.equal(game.playerTwo, 'Idiot');
   })
   describe('#gameStart()', function (){
-    it('playerOne has 26 cards', function (){
-      game.gameStart();
-      assert.equal(game.playerOneDeck.length, 26);
+    it('PlayerOne has 26 cards', function (){
+        assert.equal(game.playerOneDeck.length, 26);
     })
-    it('playerTwo has 26 cards', function (){
-      game.gameStart();
-      assert.equal(game.playerTwoDeck.length, 26);
+    it('PlayerTwo has 26 cards', function (){
+        assert.equal(game.playerTwoDeck.length, 26);
     })
     describe('#getRawValues()', function(){
-      it('face cards have raw values', function(){
-        game.gameStart();
+      it('Face cards have raw values', function(){
         let card = new Card('King', 'Clubs');
         assert.equal(card.rawValue, 13);
       })
     })
     describe('#showCards()', function (){
-      it('each player shows first card', function (){
-        game.gameStart();
+      it('Each player shows first card and places on table', function (){
         game.showCards();
         assert.equal(game.table.length == 2, true);
       })
     })
-    describe('#whoWins()', function(){
-      it('playerOne can win', function(){
-        game.gameStart();
+    describe('#possibleOutcome()', function(){
+      it('PlayerOne can win', function(){
         game.showCards();
         game.table[0].rawValue = 10;
         game.table[1].rawValue = 2;
-        game.whoWins();
-        assert.equal(game.whoWins(), "Player one wins");
+        game.possibleOutcome();
+        assert.equal(game.possibleOutcome(), "Player one wins");
       })
-      it('playerTwo can win', function (){
-        game.gameStart();
+      it('PlayerTwo can win', function (){
         game.showCards();
         game.table[0].rawValue = 2;
         game.table[1].rawValue = 10;
-        game.whoWins();
-        assert.equal(game.whoWins(), "Player two wins");
+        game.possibleOutcome();
+        assert.equal(game.possibleOutcome(), "Player two wins");
       })
-      it('a War can occur', function () {
-        game.gameStart();
+      it('A War can occur', function () {
         game.showCards();
         game.table[0].rawValue = 10;
         game.table[1].rawValue = 10;
-        game.whoWins();
-        assert.equal(game.whoWins(), "War!");
+        game.possibleOutcome();
+        assert.equal(game.possibleOutcome(), "War!");
       })
     })
-    describe('#gameOn()', function(){
-      it('winner takes both cards and puts at bottom their deck', function(){
-      game.gameStart();
+    describe('#evaluateOutcome()', function(){
+      it('Winner takes both cards and puts at bottom of their deck', function(){
       game.showCards();
       game.table[0].rawValue = 10;
       game.table[1].rawValue = 9;
-      game.gameOn();
+      game.evaluateOutcome();
       assert.equal(game.playerOneDeck.length, 27);
       })
-      it('war', function(){
-        game.gameStart();
+      it('War time', function(){
         game.showCards();
         game.table[0].rawValue = 10;
         game.table[1].rawValue = 10;
-        game.gameOn();
+        game.evaluateOutcome();
         assert.equal(game.playerOneDeck.length, 31);
       })
-      it('gameover', function (){
-        game.gameStart();
-        game.showCards();
-        game.gameOn();
-        result = "game over"
-        assert.equal(game.playerOneDeck.length, 52);
-      })
+    })
+  })
+  describe('#gameOver()', function (){
+    it('Game ends when a player has 52 cards', function (){
+      game.showCards();
+      game.gameOver();
+      game.playerTwoDeck.length = 52;
+      game.playerOneDeck.length = 52;
+      assert.equal(game.gameOver(), "Game Over");
     })
   })
 })
